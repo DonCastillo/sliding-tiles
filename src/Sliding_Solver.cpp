bb84363b5a4d91912bf2e9c4d3d8c28889bb80d4
DonCastillo <don.qcastillo@yahoo.com>
2020-04-05 04:19:59 -0600

    working program

--- src/Sliding_Solver.cpp
+++ src/Sliding_Solver.cpp
@@ -29,38 +29,44 @@ void Sliding_Solver::Solve_Puzzle()
     }
     else
     {
-        /* if not the same, get the nextconfigs of the initial board
-           and insert them into the heap */
+        // if not the same, get the nextconfigs of the initial board
+        // and insert them into the heap
         for (Board_Tile nextBoard : initBoard.nextConfigs())
         {
             nextBoard.Manhattan_Distance(goalBoard);
             tileQueue.insert(nextBoard);
         }
 
-        /* keep looking at the findMin() from the heap until
-           the findMin().getConfig == goalBoard.getConfig */
-        while(tileQueue.findMin().getConfig() != goalBoard.getConfig())
+        // keep looking at the findMin() from the heap until the findMin().getConfig == goalBoard.getConfig
+        int counter = 1, limit = 99999;
+        while( (tileQueue.findMin().getConfig() != goalBoard.getConfig()) && (counter < limit) )
         {
-
-            /* get the minimum board from the heap as determined by
-               DC = AC + EC */
+            // get the minimum board from the heap as determined by DC = AC + EC
             Board_Tile minBoard = tileQueue.findMin();
             tileQueue.deleteMin();
+            //tileQueue.makeEmpty();
 
             for (Board_Tile nextBoard : minBoard.nextConfigs())
             {
                 nextBoard.Manhattan_Distance(goalBoard);
-                //std::cout << nextBoard.getConfig() << " " << nextBoard.getDC() << " " << nextBoard.getMovesFromStart() << std::endl;
+                //std::cout << counter << ": " << nextBoard.getConfig() << " " << nextBoard.getDC() << " " << nextBoard.getMovesFromStart() << std::endl;
                 tileQueue.insert(nextBoard);
+                ++counter;
             }
-
         }
 
-        finalBoard = tileQueue.findMin();
+        // endless loop means no solution
+        if (counter >= limit) {
+            finalBoard = Board_Tile("");
+        } else {
+            finalBoard = tileQueue.findMin();
+        }
+
         tileQueue.makeEmpty();
     }
 
-    std::cout << std::endl;
+    // print result
+    std::cout << "\n\n";
     std::cout << std::left << std::setw(20) << "Start Board"
                            << std::setw(20) << "Goal Board"
                            << std::setw(20) << "Number of Moves"
@@ -69,9 +75,21 @@ void Sliding_Solver::Solve_Puzzle()
     std::cout << std::left << std::setw(20) << initBoard.getConfig()
                            << std::setw(20) << goalBoard.getConfig()
                            << std::setw(20) << finalBoard.numMoves()
-                           << std::setw(20) << finalBoard.getMovesFromStart()
-                           << std::setw(20) << std::endl;
+                           << std::setw(20);
+
+    if (finalBoard.getConfig() == initBoard.getConfig()) {
+        std::cout << "Same boards";
+        std::cout << std::setw(20) << std::endl;
+        return;
+    }
+    if (finalBoard.getConfig().length() != 0)
+        std::cout << finalBoard.getMovesFromStart();
+    else
+        std::cout << "No solution";
+    std::cout << std::setw(20) << std::endl;
 }
+
+
 
 bool Sliding_Solver::validateConfig(const std::string& config)
 {
